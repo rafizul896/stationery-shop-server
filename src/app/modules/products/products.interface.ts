0207a8a7f7915f14a08d2006d006b037208893ad
rafizul896 <rafizulislam896@gmail.com>
2024-11-22 15:45:30 +0600

    add: product interface

---
+++ src/app/modules/products/products.interface.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    name: string;
+    brand: string;
+    price: number;
+    category:  "Writing" | "Office Supplies" | "Art Supplies" | "Educational" | "Technology";
+    description: string;
+    quantity: number;
+    inStock: boolean
+}
